6a8f8241c1d8dc3d5401f669baa00855c40c01b1
Lavande <admin@5dimn.com>
2017-10-28 22:43:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,18 @@ def bar(capital):
 @app.route('/')
 def chart():
     #TODO: This will take long. Need to display sth in the front-end while fetching data
-    (capital, mycoins) = fetcher.fetch_data(fetcher.mycoins)
+    (capital, mycoins) = fetcher.fetch_data()
+    coin_no_value = {}
+    for i in mycoins.keys():
+        if i not in capital.keys(): coin_no_value[i] = mycoins[i]
+    total = 0
     for i in capital.keys():
+        total += capital[i]
         capital[i] = int(capital[i])
+    total = int(total)
     (pie_values, pie_labels, pie_length) = pie(capital)
     (bar_values, bar_labels, bar_length) = bar(capital)
-    return render_template('index.html', capital=capital, mycoins=mycoins,
+    return render_template('index.html', capital=capital, mycoins=mycoins, coin_no_value=coin_no_value, total=total,
                            pie_values=pie_values, pie_labels=pie_labels, pie_length=pie_length,
                            bar_values=bar_values, bar_labels=bar_labels, bar_length=bar_length)    
 
