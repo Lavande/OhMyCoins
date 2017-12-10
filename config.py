ab9cd52819859a34426c90589b593d24eb277ec6
Lavande <admin@5dimn.com>
2017-12-10 20:34:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,21 @@
 ##CONFIG##
-
 ##Leave it for blank if you don't want to include data from that source
+
 #Ether addresses that you'd like to watch
 addresses = ['', '', '']
+
 #Get this from Bittrex pannel. Watch-only permmision recommended
 bittrex_apikey = ''
 bittrex_apisecret = b''
+
 #Get this from Poloniex pannel. Watch-only permmision recommended
 poloniex_apikey = ''
 poloniex_apisecret = b''
+
 #Get this from Bitfinex pannel. Watch-only permmision recommended
 bitfinex_apikey = ''
 bitfinex_apisecret = b''
 
 #Mannually add balance from other sources that this program has not covered yet
-#E.g.: mycoins = {'BTC': 5.2, 'ETH': 10}
+#E.g.: other_bl = {'BTC': 5.2, 'ETH': 10}
 other_bl = {}
