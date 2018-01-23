c43447a45a809775e5d795a6baff8c6274906db1
Lavande <admin@5dimn.com>
2018-01-23 11:54:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 ##CONFIG##
 ##Leave it for blank if you don't want to include data from that source
 
-#Ether addresses that you'd like to watch
-addresses = ['', '', '']
+#Ether addresses that you'd like to watch. Input them as a list if there're more than one address.
+#E.g.: addresses = ['0xad2a6d5890c06083c340d723053b4040a28580ef', '0x214d0bb2b260b22b1498977200c1a32bdb75131b']
+addresses = []
+
 
 #Get this from Bittrex pannel. Watch-only permmision recommended
 bittrex_apikey = ''
@@ -15,6 +17,10 @@ poloniex_apisecret = b''
 #Get this from Bitfinex pannel. Watch-only permmision recommended
 bitfinex_apikey = ''
 bitfinex_apisecret = b''
+
+#Get this from Bigone settings page.
+bigone_apikey = ''
+
 
 #Mannually add balance from other sources that this program has not covered yet
 #E.g.: other_bl = {'BTC': 5.2, 'ETH': 10}
